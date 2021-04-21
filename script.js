d8586635bf13dfd745bc350452459f2cb32162ff
Shoxdiyor <sshb1712@gmail.com>
2021-04-21 15:04:49 +0500

    js3

---
+++ script.js
@@ -0,0 +1,23 @@
+const a = 1
+b = 3
+
+var z = +prompt("Delfin yordam bersinmi? 1/0")
+if(z == 1){
+        a == 2 
+} else {
+        a == 1
+}
+x = +prompt('Qirg\'oqgacha masofa')
+y = +prompt('Akuladan odamgacha masofa') + x
+if(x / a < y / b || x / c < y / b) {
+        alert ('Alive')
+} else {
+        alert('Shark ham-ham')
+}
+
+
+  
+        
+        
+        
+     
